5bab67dfcd90808e66843114ac5a8545fcc35d05
arunrajbhar123 <arunrajbhar1998@gmail.com>
2022-05-18 18:23:21 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import PayCart from './components/PayCart';
+import TextAll from './components/TextAll'
+
+function App() {
+  return (
+    <div className="App">
+     <PayCart/>
+    </div>
+  );
+}
+
+export default App;
